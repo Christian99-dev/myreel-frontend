9e486cbcd4e0ec13e5db8a0b8dcdeb98a03ab5e5
Christian99-dev <k.christian9@web.de>
2024-08-31 02:04:02 +0200

    init

---
+++ src/components/shared/Icon.tsx
@@ -0,0 +1,44 @@
+
+import ArrowDown from "@/svg/arrow-down.svg";
+import Close from "@/svg/close.svg";
+import Eye from "@/svg/eye.svg";
+import LockedClosed from "@/svg/lock-closed.svg";
+import LockOpen from "@/svg/lock-open.svg";
+import Plus from "@/svg/plus.svg";
+import Rocket from "@/svg/rocket.svg";
+import Switch from "@/svg/switch.svg";
+import Thrash from "@/svg/thrash.svg";
+import Upload from "@/svg/upload.svg";
+
+export type IconKey = "arrowDown" | "close" | "eye" | "lockClosed" | "lockOpen" | "plus" | "rocket" | "switch" | "thrash" | "upload";
+
+const iconMap: Record<
+  IconKey,
+  React.ComponentType<React.SVGProps<SVGSVGElement>>
+> = {
+  arrowDown: ArrowDown,
+  close: Close,
+  eye: Eye,
+  lockClosed: LockedClosed,
+  lockOpen: LockOpen,
+  plus: Plus,
+  rocket: Rocket,
+  switch: Switch,
+  thrash: Thrash,
+  upload: Upload,
+};
+
+const Icon = ({
+    name,
+    size = 5,
+    color = "pink_very_light",
+  }: {
+    name: IconKey;
+    size?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+    color?: "pink_very_light" | "purple_light" | "purple" | "purple_dark";
+  }) => {
+    const SvgIcon = iconMap[name];
+    return <SvgIcon strokeWidth={20} className={`fs-${size}`} fill={color} />;
+  };
+
+export default Icon
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,36 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      pink_very_light: "#E8D2FF",
+      purple_light: "#A688FA",
+      purple: "#E8D2FF",
+      purple_dark: "#E8D2FF",
+
+      user_theme_1: "#2D8FF9",
+      user_theme_2: "#A688FA",
+      user_theme_3: "#FC7FDC",
+      user_theme_4: "#FF86AE",
+      user_theme_5: "#FFA581",
+      user_theme_6: "#FFCF66",
+
+      slot_theme_1: "#0082FB",
+      slot_theme_2: "#E525A9",
+      slot_theme_3: "#FF4C7D",
+      slot_theme_4: "#FF895A",
+      slot_theme_5: "#FFC44F",
+      slot_theme_6: "#F9F871",
+      slot_theme_7: "#00A78E",
+    },
+    screens: {
+      "4xl": { min: "1921px" },
+      "3xl": { max: "1921px" },
+      "2xl": { max: "1536px" },
+      xl: { max: "1380px" },
+      lg: { max: "1024px" },
+      md: { max: "768px" },
+      sm: { max: "640px" },
+      xs: { max: "410px" },
     },
   },
   plugins: [],
