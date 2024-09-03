f340bcea4d2aa329373041797dca87b6548b8190
Christian Keller <k.christian9@web.de>
2024-09-03 23:31:38 +0200

    feat: button href and click

--- src/components/debug/DebugButton.tsx
+++ src/components/debug/DebugButton.tsx
@@ -1,43 +1,74 @@
+"use client"
 import Button from "@/components/shared/Button";
 import Icon from "@/components/shared/Icon";
 
 export default function DebugButton() {
   return (
-    <div className="w-full flex my-20 gap-10 text-center justify-center align-center">
-      <div className="flex flex-col gap-5">
-        <Button text="Button" theme="dark" />
-        <Button text="Button" />
-        <Button text="Gruppe Suchen" iconName="search" theme="dark" />
-        <Button text="Gruppe Suchen" iconName="eye" />
-        <Button text="Button" iconPosition="right" iconName="plus" theme="dark" />
-        <Button text="Button" iconPosition="right" iconName="eye" />
+    <div className="w-full flex-col flex my-20 gap-10 text-center justify-center align-center">
+
+      <div className="flex gap-5 justify-center">
+        <h1>Style</h1>
+        <div className="flex flex-col gap-5">
+          <Button text="Button" theme="dark" />
+          <Button text="Button" />
+          <Button text="Gruppe Suchen" iconName="search" theme="dark" />
+          <Button text="Gruppe Suchen" iconName="eye" />
+          <Button text="Button" iconPosition="right" iconName="plus" theme="dark" />
+          <Button text="Button" iconPosition="right" iconName="eye" />
+        </div>
+        <div className="flex flex-col gap-5">
+          <Button text="Button" theme="dark" disabled />
+          <Button text="Button" disabled />
+          <Button text="Gruppe Suchen" iconName="plus" theme="dark" disabled />
+          <Button text="Gruppe Suchen" iconName="eye" disabled />
+          <Button text="Button" iconPosition="right" iconName="plus" theme="dark" disabled />
+          <Button text="Button" iconPosition="right" iconName="eye" disabled />
+        </div>
       </div>
-      <div className="flex flex-col gap-5">
-        <Button text="Button" theme="dark" disabled />
-        <Button text="Button" disabled />
-        <Button text="Gruppe Suchen" iconName="plus" theme="dark" disabled />
-        <Button text="Gruppe Suchen" iconName="eye" disabled />
-        <Button text="Button" iconPosition="right" iconName="plus" theme="dark" disabled />
-        <Button text="Button" iconPosition="right" iconName="eye" disabled />
+
+      <div className="flex gap-5 justify-center">
+        <h1>Style</h1>
+        <div className="flex flex-col gap-5">
+          <Icon size={4} name="close" color="pink-very-light" />
+          <Icon size={4} name="eye" color="purple" />
+          <Icon size={4} name="plus" color="purple-light" />
+          <Icon size={4} name="lockOpen" color="purple-dark" />
+        </div>
+        <div className="flex flex-col gap-5">
+          <Icon size={4} hover={true} name="close" color="pink-very-light" />
+          <Icon size={4} hover={true} name="eye" color="purple" />
+          <Icon size={4} hover={true} name="plus" color="purple-light" />
+          <Icon size={4} hover={true} name="lockOpen" color="purple-dark" />
+        </div>
+        <div className="flex flex-col gap-5">
+          <Icon disabled={true} size={4} hover={true} name="close" color="pink-very-light" />
+          <Icon disabled={true} size={4} hover={true} name="eye" color="purple" />
+          <Icon disabled={true} size={4} hover={true} name="plus" color="purple-light" />
+          <Icon disabled={true} size={4} hover={true} name="lockOpen" color="purple-dark" />
+        </div>
       </div>
-      <div className="flex flex-col gap-5">
-        <Icon size={4} name="close" color="pink-very-light" />
-        <Icon size={4} name="eye" color="purple" />
-        <Icon size={4} name="plus" color="purple-light" />
-        <Icon size={4} name="lockOpen" color="purple-dark" />
+
+      <div className="flex gap-5 justify-center">
+        <h1>Functionality</h1>
+        <div className="flex flex-col gap-5">  
+          <Button text="nothing" iconPosition="right" iconName="eye" />
+          <Button onClick={() => console.log("Button click!")} href="/" text="onclick und href" iconPosition="right" iconName="eye" />
+          <Button onClick={() => console.log("Button click!")} text="onclick" iconPosition="right" iconName="eye" />
+          <Button href="/" text="href" iconPosition="right" iconName="eye" />
+        </div>
+
+        <div className="flex flex-col gap-5">  
+          <Button disabled={true} text="nothing" iconPosition="right" iconName="eye" />
+          <Button disabled={true} onClick={() => console.log("Button click!")} href="/" text="onclick und href" iconPosition="right" iconName="eye" />
+          <Button disabled={true} onClick={() => console.log("Button click!")} text="onclick" iconPosition="right" iconName="eye" />
+          <Button disabled={true} href="/" text="href" iconPosition="right" iconName="eye" />
+        </div>
       </div>
-      <div className="flex flex-col gap-5">
-        <Icon size={4} hover={true} name="close" color="pink-very-light" />
-        <Icon size={4} hover={true} name="eye" color="purple" />
-        <Icon size={4} hover={true} name="plus" color="purple-light" />
-        <Icon size={4} hover={true} name="lockOpen" color="purple-dark" />
-      </div>
-      <div className="flex flex-col gap-5">
-        <Icon disabled={true} size={4} hover={true} name="close" color="pink-very-light" />
-        <Icon disabled={true} size={4} hover={true} name="eye" color="purple" />
-        <Icon disabled={true} size={4} hover={true} name="plus" color="purple-light" />
-        <Icon disabled={true} size={4} hover={true} name="lockOpen" color="purple-dark" />
-      </div>
+
+
+
+
+
     </div>
   );
 }
--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,20 +1,25 @@
+"use client";
 import { IconKey, MainColor } from "@/types/theme";
 import Icon from "./Icon";
+import Link from "next/link";
 
 export default function Button({
   text,
   iconName,
+  onClick,
+  href,
   theme = "light",
   iconPosition = "left",
   disabled = false,
 }: {
   text: string;
   iconName?: IconKey;
+  onClick?: () => any;
+  href?: string;
   theme?: "light" | "dark";
   iconPosition?: string;
   disabled?: boolean;
 }) {
-
   // Parameters
   const smallerPadding = iconName != undefined;
 
@@ -44,41 +49,67 @@ export default function Button({
   const padding = smallerPadding
     ? "py-[--spacing-2] px-[--spacing-5]"
     : "py-[--spacing-2] px-[--spacing-8]";
-  const opacity = disabled ? "opacity-50" : "opacity-100"  
-  const cursor = disabled ? "cursor-not-allowed" : "cursor-pointer"
+  const opacity = disabled ? "opacity-50" : "opacity-100";
+  const cursor = disabled ? "cursor-not-allowed" : "cursor-pointer";
 
-  return (
-    <button
-      className={`
-        ${disabled ? `` : `group`}
-        self-start
-        rounded-main 
-        flex
-        fs-9 
-        gap-[--spacing-2]  
-        font-medium
-        transition-colors duration-200 ease-in-out
-
-        ${padding}
-        ${opacity}
-        ${cursor}
-        
-        text-${style[theme].font}
-        ${disabled ? `` : `hover:text-${style[theme].fontHover}`}
-
-        bg-${style[theme].background}
-        ${disabled ? `` : `hover:bg-${style[theme].backgroundHover}`}
-        
-      `}
-      disabled={disabled}
-    >
+  const content = (
+    <>
       {iconName && iconPosition === "left" && (
-        <Icon groupHover={true} name={iconName} size={9} color={style[theme].font} />
+        <Icon
+          groupHover={true}
+          name={iconName}
+          size={9}
+          color={style[theme].font}
+        />
       )}
       {text}
       {iconName && iconPosition === "right" && (
-        <Icon groupHover={true} name={iconName} size={9} color={style[theme].font} />
+        <Icon
+          groupHover={true}
+          name={iconName}
+          size={9}
+          color={style[theme].font}
+        />
       )}
+    </>
+  );
+
+  const classNames = `
+    ${disabled ? `` : `group`}
+    self-start
+    rounded-main 
+    flex
+    fs-9 
+    gap-[--spacing-2]  
+    font-medium
+    transition-colors duration-200 ease-in-out
+
+    ${padding}
+    ${opacity}
+    ${cursor}
+
+    text-${style[theme].font}
+    ${disabled ? `` : `hover:text-${style[theme].fontHover}`}
+
+    bg-${style[theme].background}
+    ${disabled ? `` : `hover:bg-${style[theme].backgroundHover}`}
+  `;
+
+  if (href && !disabled) {
+    return (
+      <Link href={href} className={classNames}>
+        {content}
+      </Link>
+    );
+  }
+
+  return (
+    <button
+      onClick={onClick}
+      className={classNames}
+      disabled={disabled}
+    >
+      {content}
     </button>
   );
 }
