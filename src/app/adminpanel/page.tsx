454e84dd969eb31abe1d176af4f98b4a6e3d4f61
Christian Keller <k.christian9@web.de>
2024-09-26 15:31:18 +0200

    feat: panelLayout component

---
+++ src/app/adminpanel/page.tsx
@@ -0,0 +1,31 @@
+import PanelLayout from "@/components/shared/PanelLayout";
+
+export default function Page() {
+  return (
+    <PanelLayout title="Admin Panel">
+      <>
+        <div className="size-20 bg-user-theme-3">testing</div>
+        <div className="size-20 bg-user-theme-3">testing</div>
+        <div className="size-20 bg-user-theme-3">testing</div>
+      </>
+      <>
+        <div className="size-20 bg-user-theme-3">testing</div>
+        <div className="size-20 bg-user-theme-3">testing</div>
+        <div className="size-20 bg-user-theme-3">testing</div>
+        <div className="size-20 bg-user-theme-3">testing</div>
+        <div className="size-20 bg-user-theme-3">testing</div>
+        <div className="size-20 bg-user-theme-3">testing</div>
+        <div className="size-20 bg-user-theme-3">testing</div>
+        <div className="size-20 bg-user-theme-3">testing</div>
+        <div className="size-20 bg-user-theme-3">testing</div>
+        <div className="size-20 bg-user-theme-3">testing</div>
+        <div className="size-20 bg-user-theme-3">testing</div>
+        <div className="size-20 bg-user-theme-3">testing</div>
+        <div className="size-20 bg-user-theme-3">testing</div>
+        <div className="size-20 bg-user-theme-3">testing</div>
+        <div className="size-20 bg-user-theme-3">testing</div>
+        <div className="size-20 bg-user-theme-3">testing</div>
+      </>
+    </PanelLayout>
+  );
+}
---
+++ src/components/shared/PanelLayout.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from "react";
+
+export default function PanelLayout({
+  title,
+  children,
+}: {
+  title: string;
+  children: ReactNode[];
+}) {
+  return (
+    <div className="h-screen flex flex-col">
+      {/** TopBar */}
+      <div className="bg-purple-very-dark">
+        <h1 className="fs-9 text-pink-very-light text-center font-bold p-[--spacing-5] border-b border-b-purple-light">
+          {title}
+        </h1>
+      </div>
+
+      {/** Side Bar und Main Content */}
+
+      <div className="bg-purple-very-dark flex flex-1">
+        {/** Side Bar */}
+        <div className="border-r border-r-purple-light w-[10%] p-[--spacing-7] relative ios-scrollbar">
+          <div className="absolute top-0 left-0 right-0 bottom-0 p-[--spacing-7] overflow-y-auto">
+            {children[0]}
+          </div>
+        </div>
+
+        {/** Main Content */}
+        <div className="flex-1 bg-purple-dark relative">
+          <div className="absolute top-0 left-0 right-0 bottom-0 p-[--spacing-7] overflow-y-auto ios-scrollbar">
+            {children[1]}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
